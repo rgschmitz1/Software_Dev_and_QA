d95282fa9738d24b786d3dd9c39322e4baebdcb4
ibokov <ilyabokov@gmail.com>
2020-01-22 12:48:50 -0800

    Testing

--- src/weather/WeatherController.java
+++ src/weather/WeatherController.java
@@ -1,6 +1,6 @@
 package weather;
 import java.nio.ByteBuffer;
-// Testing Git from IntelliJ -Ilya 
+//Another Test
 public class WeatherController implements Runnable {
 	private static final int TEMP_OFFSET = 12;
 	private static final int HUMID_OFFSET = 33;
